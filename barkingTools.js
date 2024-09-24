220652fe918961e0876a174155778b390a048a07
Steve Craig <wolfpacksteve@gmail.com>
2024-09-24 11:50:12 -0400

    fix missing await

--- barkingTools.js
+++ barkingTools.js
@@ -240,7 +240,7 @@ async function checkBarkPower() {
 
                 // Fetch barksReceived from leaderboard
                 console.log('Fetching barksReceived from leaderboard...');
-                let leaderboardData = BarkApi.fetchBarksReceviedLeaderboard();
+                let leaderboardData = await BarkApi.fetchBarksReceviedLeaderboard();
 
                 // Find the twitterHandle in the leaderboard data
                 let found = false;
